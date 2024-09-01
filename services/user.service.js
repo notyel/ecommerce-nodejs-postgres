8e8c8e574ee54a49aca614f3d365a3867349786c
Leyton M. Espitia Diaz <leyton_espitia@hotmail.com>
2024-09-01 02:31:19 -0500

    Implementación del crud de usuarios

--- services/user.service.js
+++ services/user.service.js
@@ -1,31 +1,36 @@
-//const boom = require('@hapi/boom');
+const boom = require('@hapi/boom');
 const { models } = require('./../libs/sequelize');
 
 class UserService {
   constructor() {}
 
   async create(data) {
-    return data;
+    const newUser = await models.User.create(data);
+    return newUser;
   }
 
   async find() {
-    console.log(models);
     const response = await models.User.findAll();
     return response;
   }
 
   async findOne(id) {
-    return { id };
+    const response = await models.User.findByPk(id);
+    if (!response) {
+      throw boom.notFound('user not found');
+    }
+    return response;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const user = await this.findOne(id);
+    const response = await user.update(changes);
+    return response;
   }
 
   async delete(id) {
+    const user = await this.findOne(id);
+    await user.destroy();
     return { id };
   }
 }
